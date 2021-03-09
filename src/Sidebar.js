28fe7fda22644016da05e51e84ef1f34ef5918a4
Vinod Kumar R <47006421+vk0808@users.noreply.github.com>
2021-03-09 23:32:53 +0530

    added sidebar links

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -3,9 +3,31 @@ import { links, social } from './data';
 
 const Sidebar = () => {
   return (
-    <>
-      <p>Hello</p>
-    </>
+    <aside classNmae={`sidebar show-sidebar`}>
+      <div className="sidebar-container">
+        <button className="close-btn"><FaTimes /></button>
+      </div>
+      <ul className="links">
+        {links.map(link => {
+          const {id,url,text,icon} = link;
+          return (
+            <li key={id}>
+              <a href={url}>{icon} {text}</a>
+            </li>
+          );
+        })}
+      </ul>
+      <ul className="social-icons">
+        {social.map(link => {
+          const {id, url, icon} = link;
+          return (
+            <li key={id}>
+              <a href={url}>{icon}</a>
+            </li>
+          );
+        })}
+      </ul>
+    </aside>
   );
 };
 
